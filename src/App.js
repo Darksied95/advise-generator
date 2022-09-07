d26d54cdfee486bb6c52580bd86f890e224f950d
Darksied95 <aliyuefe9501@gmail.com>
2022-09-07 21:28:33 +0100

    added functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import diceLogo from "./assets/icon-dice.svg";
 import mobileLogo from "./assets/pattern-divider-mobile.svg";
 import desktopLogo from "./assets/pattern-divider-desktop.svg";
+import "./App.css";
 function App() {
+  const [{ id, advice }, setData] = useState({});
+  // const [getAdvice, setGetAdvice] = useState(false);
+
+  function getAdvise() {
+    const getRandomNumber = () => Math.ceil(Math.random() * 224);
+    const adviceApi = `https://api.adviceslip.com/advice/${getRandomNumber()}`;
+    fetch(adviceApi)
+      .then((response) => response.json())
+      .then(({ slip }) => setData(slip));
+  }
+
+  useEffect(() => {
+    getAdvise();
+  }, []);
+
   return (
     <main>
-      <h1>ADVICE #117</h1>
-      <q>
-        It is easy to sit up and take notice, what's difficult is getting up and
-        taking action.
-      </q>
+      <h1>ADVICE #{id}</h1>
+      <q>{advice}</q>
       <img src={mobileLogo} alt={"divider"} className="mobile-logo divider" />
       <img src={desktopLogo} alt={"divider"} className="desktop-logo divider" />
-      <div className="dice-icon-container">
+      <div className="dice-icon-container" onClick={getAdvise}>
         <img src={diceLogo} alt="dice-icon" className="dice-icon" />
       </div>
     </main>
